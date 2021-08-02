e3229513030d345ae2c6b11f54cf6553a2ccdd95
OrkasST <egor31@outlook.com>
2021-08-02 23:37:50 +0300

    +Messages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Messages from './components/Messages/Messages';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      {/*<Profile />*/}
+      <Messages />
     </div>
   )
 }
---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Messages.module.css';
+
+const Messages = (props) => {
+  return (
+    <div className={s.content}>
+      Messages
+    </div>
+  )
+}
+
+export default Messages;
