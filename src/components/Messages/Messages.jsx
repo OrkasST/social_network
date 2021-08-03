883cfd41350b2cdb4abb682006d330709ce0cd79
OrkasST <egor31@outlook.com>
2021-08-03 18:19:44 +0300

    dialog modif

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Messages.module.css';
 
 const Messages = (props) => {
   return (
-    <div>
-      Messages
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <div className={s.dialog}>
+          <NavLink to='/dialogs/1'>Andry</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to='/dialogs/2'>Nick</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to='/dialogs/3'>Anny</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to='/dialogs/4'>Andrea</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to='/dialogs/5'>Mikle</NavLink>
+        </div>
+      </div>
+
+      <div className={s.dialogMessages}>
+        <div className={s.message}>Hi!</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>What do u do today evening?</div>
+      </div>
     </div>
   )
 }
